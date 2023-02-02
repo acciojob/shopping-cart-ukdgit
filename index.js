840f467efd44573cc82fc09cfaffa21e3c7ed2d5
Umakant Dwivedi <78226651+ukdgit@users.noreply.github.com>
2023-02-02 20:30:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-const app =require('./app.js');
-app.listen('3000',()=>{
-  console.log("Listening on port 3000");
+const express = require('express');
+const path = require('path');
+const app =express();
+
+app.use(express.static(__dirname));
+app.get('/',(req,res)=>{
+  res.sendFile(path.join(__dirname+'/main.html'));
 })
+
+module.exports = app;
